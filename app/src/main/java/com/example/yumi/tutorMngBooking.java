a9e85c37c050d4446186d935a28267669e08c550
yujjinn <35555129+yujjinn@users.noreply.github.com>
2019-11-08 02:45:37 +0900

    Update tutorMngBooking.java

--- app/src/main/java/com/example/yumi/tutorMngBooking.java
+++ app/src/main/java/com/example/yumi/tutorMngBooking.java
@@ -1,16 +1,389 @@
 package com.example.yumi;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.CompoundButton;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class tutorMngBooking extends AppCompatActivity {
+
+
+    private static String TAG = "phptest_MainActivity";
+    private static final String TAG_JSON="webnautes";
+    ListView mlistView;
+    String mJsonString, uJsonString;
+    ArrayList<HashMap<String, String>> mArrayList;
+    private static final String ID = "id";
+    private static final String TAG_SID = "s_id";
+    private static final String BOOK = "book";
+    private static final String sTime = "start_time";
+    private static final String eTime = "end_time";
+    String table_adr= "readyForBooking";
+    TextView mTextViewResult, uTextViewResult;
+    Switch aSwitch;
+    phpConnect task;
+    phpUpdate uptTask;
+    phpCancel ccTask;
+    int arr_id[];
+    String arr_sid[]; // s_id 저장 배열
+    String st_time[];
+    String end_time[];
+    int index_num=0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.tutor_mng_booking);
+        mlistView = (ListView)findViewById(R.id.listView_booking_list) ;
+        task = new phpConnect();
+        task.execute();
 
+        mlistView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                getMoreBooking(position);
+            }
+        });
+
+
+        aSwitch = (Switch) findViewById(R.id.bookSwitch);
+
+        //스위치 클릭
+        aSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked == true) {
+                    table_adr = "alreadyDoneBooking";
+                    task = new phpConnect();
+                    task.execute();
+
+                    mlistView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                        @Override
+                        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                            editBooking(position);
+                        }
+                    });
+
+                } else {
+                    table_adr= "readyForBooking";
+                    task = new phpConnect();
+                    task.execute();
+
+                    mlistView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                        @Override
+                        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                            getMoreBooking(position);
+                        }
+                    });
+                }
+            }
+        });
+
+
+    }
+
+    void editBooking(int position) {
+        index_num = position;
+
+        new AlertDialog.Builder(tutorMngBooking.this)
+                .setTitle("예약 정보" )
+                .setMessage("\n학생 정보 : " + arr_sid[position] +"\n"+"예약시간 : " + st_time[position]+" ~ " + end_time[position])
+                .setPositiveButton("대화하기", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // TODO Auto-generated method stub
+                        Toast.makeText(tutorMngBooking.this, "대화 창으로 넘어갑니다.", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .setNeutralButton("예약 취소하기", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // TODO Auto-generated method stub
+                        ccTask = new phpCancel();
+                        ccTask.execute();
+                        Toast.makeText(tutorMngBooking.this, "예약을 취소하셨습니다.", Toast.LENGTH_SHORT).show();
+
+                    }
+                })
+                .show();
+    }
+
+    void getMoreBooking(int position) {
+        index_num = position;
+
+        new AlertDialog.Builder(tutorMngBooking.this)
+                .setTitle("예약하기" )
+                .setMessage("\n학생 정보 : " + arr_sid[position])
+                .setPositiveButton("대화하기", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // TODO Auto-generated method stub
+                        Toast.makeText(tutorMngBooking.this, "대화 창으로 넘어갑니다.", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .setNeutralButton("예약하기 ("+ st_time[position]+" ~ "+end_time[position]+")", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // TODO Auto-generated method stub
+                        uptTask = new phpUpdate();
+                        uptTask.execute();
+                        Toast.makeText(tutorMngBooking.this, "예약이 완료되었습니다.", Toast.LENGTH_SHORT).show();
+
+                    }
+                })
+                .show();
+    }
+
+
+    class phpConnect extends AsyncTask<String,Void,String> {
+
+        @Override
+        protected String doInBackground(String... arg0) {
+            try {
+                String link = "http://1.234.38.211/"+table_adr+".php";
+                URL url = new URL(link);
+                HttpClient client = new DefaultHttpClient();
+                HttpGet request = new HttpGet();
+                request.setURI(new URI(link));
+                HttpResponse response = client.execute(request);
+                BufferedReader in = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+
+                StringBuffer sb = new StringBuffer("");
+                String line = "";
+
+                while ((line = in.readLine()) != null) {
+                    sb.append(line);
+                }
+
+                in.close();
+                return sb.toString();
+
+            } catch (Exception e) {
+                return new String("Exception: " + e.getMessage());
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String result){
+            super.onPostExecute(result);
+
+            Log.d(TAG, "response  - " + result);
+
+            if (result == null){
+
+                mTextViewResult.setText("error occurred");
+            }
+            else {
+                mJsonString = result;
+
+                showResult();
+            }
+            //txtview.setText(result);
+        }
+    }
+
+    class phpUpdate extends AsyncTask<String, Void, String>{
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+
+            String postParameters = "id=" + arr_id[index_num];
+
+            try {
+                URL url = new URL("http://1.234.38.211/ttUpdateBooking.php");
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+
+                httpURLConnection.setReadTimeout(5000);
+                httpURLConnection.setConnectTimeout(5000);
+                httpURLConnection.setRequestMethod("POST");
+                httpURLConnection.connect();
+
+                OutputStream outputStream = httpURLConnection.getOutputStream();
+                outputStream.write(postParameters.getBytes("UTF-8"));
+                outputStream.flush();
+                outputStream.close();
+
+                int responseStatusCode = httpURLConnection.getResponseCode();
+                InputStream inputStream;
+                if(responseStatusCode == HttpURLConnection.HTTP_OK) {
+                    inputStream = httpURLConnection.getInputStream();
+                }
+                else{
+                    inputStream = httpURLConnection.getErrorStream();
+                }
+
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream, "UTF-8");
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+                StringBuilder sb = new StringBuilder();
+                String line = null;
+
+                while((line = bufferedReader.readLine()) != null){
+                    sb.append(line);
+                }
+                bufferedReader.close();
+                return sb.toString();
+            } catch (Exception e) {
+                return new String("Error: " + e.getMessage());
+            }
+
+        }
+    }
+
+    class phpCancel extends AsyncTask<String, Void, String>{
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+
+            String postParameters = "id=" + arr_id[index_num];
+
+            try {
+                URL url = new URL("http://1.234.38.211/cancelBooking.php");
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+
+                httpURLConnection.setReadTimeout(5000);
+                httpURLConnection.setConnectTimeout(5000);
+                httpURLConnection.setRequestMethod("POST");
+                httpURLConnection.connect();
+
+                OutputStream outputStream = httpURLConnection.getOutputStream();
+                outputStream.write(postParameters.getBytes("UTF-8"));
+                outputStream.flush();
+                outputStream.close();
+
+                int responseStatusCode = httpURLConnection.getResponseCode();
+                InputStream inputStream;
+                if(responseStatusCode == HttpURLConnection.HTTP_OK) {
+                    inputStream = httpURLConnection.getInputStream();
+                }
+                else{
+                    inputStream = httpURLConnection.getErrorStream();
+                }
+
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream, "UTF-8");
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+                StringBuilder sb = new StringBuilder();
+                String line = null;
+
+                while((line = bufferedReader.readLine()) != null){
+                    sb.append(line);
+                }
+                bufferedReader.close();
+                return sb.toString();
+            } catch (Exception e) {
+                return new String("Error: " + e.getMessage());
+            }
+
+        }
+    }
+
+
+
+    private void showResult(){
+
+        mArrayList = new ArrayList<>();
+        try {
+            JSONObject jsonObject = new JSONObject(mJsonString);
+            JSONArray jsonArray = jsonObject.getJSONArray(TAG_JSON);
+
+            arr_id = new int[jsonArray.length()];
+            arr_sid = new String[jsonArray.length()];
+            st_time = new String[jsonArray.length()];
+            end_time = new String[jsonArray.length()];
+
+
+            for(int i=0;i<jsonArray.length();i++){
+
+                JSONObject item = jsonArray.getJSONObject(i);
+
+
+                int id_num = item.getInt(ID);
+                String bookName = item.getString(BOOK);
+                String startTime = item.getString(sTime);
+                String endTime = item.getString(eTime);
+                String st_id = item.getString(TAG_SID);
+
+
+                HashMap<String,String> hashMap = new HashMap<>();
+
+                arr_id[i]=id_num;
+                arr_sid[i]=st_id;
+                st_time[i]=startTime;
+                end_time[i]=endTime;
+
+
+
+                hashMap.put(BOOK , bookName);
+                hashMap.put(sTime, startTime);
+                hashMap.put(eTime, endTime);
+
+
+                mArrayList.add(hashMap);
+            }
+
+
+            ListAdapter adapter = new SimpleAdapter(
+                    tutorMngBooking.this, mArrayList, R.layout.tutor_booking_list,
+                    new String[]{BOOK, sTime, eTime},
+                    new int[]{R.id.textView_list_book, R.id.booking_start_time, R.id.booking_end_time}
+            );
+
+            mlistView.setAdapter(adapter);
+
+        } catch (JSONException e) {
+
+            Log.d(TAG, "showResult : ", e);
+        }
 
     }
 }
