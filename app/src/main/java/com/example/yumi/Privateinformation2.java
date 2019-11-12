1162903634756c62f6c1eb7cbd2d94697ed21d61
SUPINKIM <49034615+SUPINKIM@users.noreply.github.com>
2019-11-12 18:42:19 +0900

    Update Privateinformation2.java
    학생 가입화면2 코드 수정

--- app/src/main/java/com/example/yumi/Privateinformation2.java
+++ app/src/main/java/com/example/yumi/Privateinformation2.java
@@ -4,6 +4,7 @@ import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
@@ -13,7 +14,9 @@ import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -42,6 +45,15 @@ public class Privateinformation2 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setTitle("회원정보 입력하기");
         setContentView(R.layout.activity_privateinformation_student);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayShowCustomEnabled(true); //커스터마이징 하기 위해 필요
+        actionBar.setDisplayShowTitleEnabled(true);
+        actionBar.setDisplayHomeAsUpEnabled(true); // 뒤로가기 버튼, 디폴트로 true만 해도 백버튼이 생김
+
 
         Button nickname_check = (Button) findViewById(R.id.id_check3);
         nickname_check.setOnClickListener(new View.OnClickListener() {
@@ -129,11 +141,16 @@ public class Privateinformation2 extends AppCompatActivity {
         }
     }
 
-    public void goBack2(View view){ //뒤로 가기
-        if(view.getId()==R.id.cancel){
-            Intent intent = new Intent(getApplicationContext(), Studentsignup.class);
-            startActivity(intent);
+    //앱바 메뉴 클릭 이벤트
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                // User chose the "Settings" item, show the app settings UI...
+                super.onBackPressed();
+                return true;
         }
+        return super.onOptionsItemSelected(item);
     }
 
     public class RegisterDB extends AsyncTask<Void, Integer, String> {
@@ -275,10 +292,10 @@ public class Privateinformation2 extends AppCompatActivity {
             String check = item.getString(TAG_CHECK);
 
             if (check.equals("1")) {
-                Toast.makeText(getApplicationContext(),"이 닉네임는 안됩니다.", Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(),"중복된 닉네임이 존재합니다.", Toast.LENGTH_SHORT).show();
             }
             if (check.equals("0")) {
-                Toast.makeText(getApplicationContext(),"이 닉네임은 가능합니다.", Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(),"가능한 닉네임입니다.", Toast.LENGTH_SHORT).show();
             }
 
         } catch (JSONException e) {
