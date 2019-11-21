60aff26eff919c4ae94dcb64bef8f9a3556831a7
SUPINKIM <49034615+SUPINKIM@users.noreply.github.com>
2019-11-22 00:45:48 +0900

    Update MenuActivity.java
    로그아웃 후 다시 이전 상태로 복원 불가하는 코드 추가

--- app/src/main/java/com/example/yumi/MenuActivity.java
+++ app/src/main/java/com/example/yumi/MenuActivity.java
@@ -65,4 +65,5 @@ public class MenuActivity extends Activity {
                 throw new IllegalStateException("Unexpected value: " + view.getId());
         }
     }
+    @Override public void onBackPressed() { }
 }
