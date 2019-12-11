54f3664954dc7ebf3687676798ae4a2b3c11afc9
SUPINKIM <49034615+SUPINKIM@users.noreply.github.com>
2019-12-11 14:43:04 +0900

    Update stdMyPage.java

--- app/src/main/java/com/example/yumi/stdMyPage.java
+++ app/src/main/java/com/example/yumi/stdMyPage.java
@@ -110,18 +110,28 @@ public class stdMyPage extends AppCompatActivity {
         yyyy = yearFormat.format(currentTime);
         mm = monthFormat.format(currentTime);
         dd = dayFormat.format(currentTime);
+        if (dd.substring(0,1).equals("0")){
+            dd = dd.substring(1,2);
+        }
 
         task = new stdMyPage.phpConnect();
         task.execute();
 
-        if (listNum > 0) {
-            mlistView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                @Override
-                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                    getMoreBooking(position);
+        mlistView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                try {
+                    Intent intent = new Intent(getApplicationContext(), StudentQuestionDetailActivity.class);
+                    intent.putExtra("question_id", arr_id[position]);
+                    startActivity(intent); //문제 상세 정보 띄어주기 액티비티
+                    //getMoreBooking(position);
                 }
-            });
-        }
+                catch (Exception e){
+
+                }
+            }
+        });
+
         new Thread(new Runnable() {
             @Override public void run() {
                 BottomBar bottomBar = findViewById(R.id.bottomBar);
@@ -138,7 +148,7 @@ public class stdMyPage extends AppCompatActivity {
                             startActivity(intent);
                         }
                         else if (tabId == R.id.tab_chatting_log){
-                            Intent intent = new Intent(getApplicationContext(), stdManageTT.class);
+                            Intent intent = new Intent(getApplicationContext(), stdPreferences.class);
                             startActivity(intent);
                         }
                     }
@@ -148,10 +158,8 @@ public class stdMyPage extends AppCompatActivity {
 
     }
 
-
     @Override public void onBackPressed() { //super.onBackPressed();
     }// 뒤로 가기 막기
-
 
     void getMoreBooking(int position) {
         index_num = position;
@@ -178,12 +186,13 @@ public class stdMyPage extends AppCompatActivity {
 
 
     class phpConnect extends AsyncTask<String,Void,String> {
-        String stringParameter = "&yyyy="+yyyy+"&mm="+mm+"&dd="+dd;
+        String stringParameter = "&yyyy="+yyyy+"년&mm="+mm+"월&dd="+dd;
 
         @Override
         protected String doInBackground(String... arg0) {
             try {
                 String link = "http://1.234.38.211/todayClass.php?id="+sid+stringParameter;
+
                 URL url = new URL(link);
                 HttpClient client = new DefaultHttpClient();
                 HttpGet request = new HttpGet();
