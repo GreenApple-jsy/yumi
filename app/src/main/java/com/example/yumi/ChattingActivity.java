0e007fbf8b4075e5112324b52631f151db1c457e
SUPINKIM <49034615+SUPINKIM@users.noreply.github.com>
2019-12-18 16:00:53 +0900

    Update ChattingActivity.java

--- app/src/main/java/com/example/yumi/ChattingActivity.java
+++ app/src/main/java/com/example/yumi/ChattingActivity.java
@@ -3,9 +3,10 @@ package com.example.yumi;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ListView;
@@ -84,6 +85,12 @@ public class ChattingActivity extends AppCompatActivity {
                 message.setText((""));
             }
         });
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.connect_menu,menu);
+        return true;
     }
 
     @Override
